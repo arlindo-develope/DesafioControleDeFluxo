bd21f63e5789c0da78e1d8e8c89cfdc198bc3bde
arlindo-develope <arlindo.develope@gmail.com>
2024-06-20 04:48:12 -0300

    Segundo comit

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
